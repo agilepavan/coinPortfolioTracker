a26913c75268c1ee4a978d221bddb2bb0d2ed2df
aurorekha <rekhabhagavatula89@gmail.com>
2018-05-16 16:28:42 +0100

    Add onchange and other functions for the input
    And also replace the input component with plain inputs.

--- app/components/SearchForm.jsx
+++ app/components/SearchForm.jsx
@@ -6,30 +6,52 @@ import Button from '../../app/components/shared/Button.jsx';
 import '../../src/styles/scss/components/Input.scss';
 
 class SearchForm extends Component {
-  onFormSubmit(e){
-    e.preventDefault();
+  constructor() {
+    super();
+    this.state = {
+      apiKey: '',
+      apiSecret: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
   }
 
   render() {
     return (
-      <div>
-        <form onSubmit={this.onFormSubmit}>
-          <Input
-            id="api-key"
-            className="inputs"
-            label="Your Api Key"
-            ref="apiKey"
-          />
-          <Input
-            id="api-secret"
-            className="inputs"
-            label="Your Secret Value"
-            ref="apiSecret"
-          />
-          <Button>Get Coins</Button>
-        </form>
-      </div>
-  )
+    <form onSubmit={this.handleSubmit}>
+      <label>
+        Your Api Key
+        <input
+          id="api-key"
+          className="inputs"
+          name="apiKey"
+          onChange={this.handleChange}
+        />
+      </label>
+      <br />
+      <label>
+        Your Api Secret
+        <input
+          id="api-secret"
+          className="inputs"
+          name="apiSecret"
+          onChange={this.handleChange}
+        />
+      </label>
+      <br />
+      <Button type="submit">Get Coins</Button>
+    </form>
+    )
   }
 };
 
