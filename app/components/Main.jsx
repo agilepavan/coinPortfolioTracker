a16806e9e7a31d7cadf1dbc0c5db5c11eeffa8c4
aurorekha <rekhabhagavatula89@gmail.com>
2018-06-06 15:07:00 +0100

    Add dropown links etcs to make it more like a normal nav

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import { NavLink, IndexLink, Link } from 'react-router-dom';
-import { H3 } from '../../app/components/shared/Header.jsx'
+import Nav from '../../app/components/shared/Nav';
+import { H3 } from '../../app/components/shared/Header.jsx';
+
+// Load foundation
+require ('style-loader!css-loader!foundation-sites/dist/css/foundation.min.css');
+require('style-loader!css-loader!foundation-sites/dist/css/foundation-float.min.css');
+require('script-loader!jquery/dist/jquery.min.js');
+require('script-loader!foundation-sites/dist/js/foundation.min.js');
+$(document).foundation();
+
 const Main = (props) => {
   return (
     <div>
+      <Nav />
+      <br/>
       <div className="row">
         <div className="columns medium-8 large-8 small-centered">
           <H3>Crypto Coin Portfolio</H3>
---
+++ app/components/shared/Nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {NavLink, IndexLink, Link} from 'react-router-dom';
+import Links from './Links';
+
+const Nav = () => {
+  return(
+    <div>
+      <Links/>
+    </div>
+  )
+}
+export default Nav;
